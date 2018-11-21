dcd5af0e4d1e48f581001aa2138cab909efcff2a
f <f@Hp-Fede.lan>
2018-11-21 11:47:19 +0100

    soluzione

--- src/it/polito/tdp/babs/BabsController.java
+++ src/it/polito/tdp/babs/BabsController.java
@@ -2,9 +2,12 @@ package it.polito.tdp.babs;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.babs.model.CountResult;
 import it.polito.tdp.babs.model.Model;
+import it.polito.tdp.babs.model.SimulationResult;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
@@ -36,12 +39,44 @@ public class BabsController {
 
 	@FXML
 	void doContaTrip(ActionEvent event) {
+		
+		try {
+			txtResult.clear();
+			LocalDate date = pickData.getValue();
+			List<CountResult> results = model.getTripCounts(date);
 
+			if (results == null) {
+				txtResult.setText("Non ci sono trip per la data selezionata");
+				return;
+			}
+
+			for (CountResult cc : results) {
+				txtResult.appendText(cc.toString());
+			}
+			
+		} catch (RuntimeException e) {
+			txtResult.setText("Errore di connession al DB");
+		}
 	}
 
 	@FXML
 	void doSimula(ActionEvent event) {
-
+		try {
+			txtResult.clear();
+			LocalDate date = pickData.getValue();
+			
+			if (date.getDayOfWeek().getValue() > 4) {
+				txtResult.setText("Selezionare una data dal Lun al Ven");
+				return;
+			}
+			
+			Double k = sliderK.getValue();
+			SimulationResult sr = model.simula(date, k);
+			txtResult.setText(sr.toString());
+			
+		} catch (RuntimeException e) {
+			txtResult.setText("Errore di connession al DB");
+		}
 	}
 
 	@FXML
